842172a71ca418bd21a62b8cd5112a41bfc32430
Firdaus <firdausmaulana@akupintar.id>
2022-10-05 20:49:23 +0700

    Create SongService

---
+++ src/services/postgres/SongsService.js
@@ -0,0 +1,84 @@
+const { nanoid } = require('nanoid')
+const { Pool } = require('pg')
+const InvariantError = require('../../exceptions/InvariantError')
+const NotFoundError = require('../../exceptions/NotFoundError')
+const { mapDBtoModelSong } = require('../../utils')
+
+class SongsService {
+  constructor () {
+    this._pool = new Pool()
+  }
+
+  async addSong ({ title, year, genre, performer, duration, albumId }) {
+    const id = 'song-' + nanoid(16)
+    const createdAt = new Date().toISOString()
+    const updatedAt = createdAt
+
+    // Kirim data ke DB menggunakan SQL
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId, createdAt, updatedAt]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Song gagal ditambahkan')
+    }
+
+    return result.rows[0].id
+  }
+
+  async getSongs () {
+    const result = await this._pool.query('SELECT * FROM songs')
+    return result.rows.map(mapDBtoModelSong)
+  }
+
+  async getSongById (id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id]
+    }
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Song tidak ditemukan')
+    }
+
+    return result.rows.map(mapDBtoModelSong)[0]
+  }
+
+  async editSongById (id, { title, year, genre, performer, duration, albumId }) {
+    const updatedAt = new Date().toISOString()
+    // Kirim data ke DB menggunakan SQL
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6, updated_at = $7 WHERE id = $8 RETURNING id',
+      values: [title, year, genre, performer, duration, albumId, updatedAt, id]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Song tidak ditemukan')
+    }
+
+    return result.rows[0].id
+  }
+
+  async deleteSongById (id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+      values: [id]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Song gagal dihapus. Id tidak ditemukan')
+    }
+
+    return result
+  }
+}
+
+module.exports = SongsService
--- src/utils/index.js
+++ src/utils/index.js
@@ -20,7 +20,7 @@ const mapDBtoModelSong = ({
   performer,
   genre,
   duration,
-  albumId,
+  album_id,
   created_at,
   updated_at
 }) => ({
@@ -30,7 +30,7 @@ const mapDBtoModelSong = ({
   performer,
   genre,
   duration,
-  albumId,
+  albumId: album_id,
   createdAt: created_at,
   updatedAt: updated_at
 })
